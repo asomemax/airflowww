ea619e1cca6f53f001c7e6a0ec966f905e455fa1
Thomas <thmsphung2000@gmail.com>
2018-05-25 20:58:41 -0700

    Added Calculate class

---
+++ src/airflowww/Calculate.java
@@ -0,0 +1,16 @@
+package airflowww;
+
+// this class will be responsible for all the calculations
+public class Calculate {
+	
+	// reference: https://en.wikipedia.org/wiki/Drag_coefficient
+	// flowSpeed == power of wind?
+	public static double dragCoeff(double dragForce, double massDensity, double flowSpeed, double area) {
+		return (2 * dragForce) / (massDensity * Math.pow(flowSpeed, 2) * area);
+	}
+	
+	// reference: https://en.wikipedia.org/wiki/Drag_equation
+	public static double drag(double massDensity, double flowSpeed, double dragCoeff, double area) {
+		return 0.5 * massDensity * Math.pow(flowSpeed,  2) * dragCoeff * area;
+	}
+}
