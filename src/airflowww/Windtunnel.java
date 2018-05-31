957d6df86e8a55689a8983dedda3475977167a35
Thomas <thmsphung2000@gmail.com>
2018-05-30 20:12:58 -0700

    making Windtunnel a subclass of figure

--- src/airflowww/Windtunnel.java
+++ src/airflowww/Windtunnel.java
@@ -1,29 +1,34 @@
 package airflowww;
 
-public class Windtunnel {
+public class Windtunnel extends Figure { 
 	public double angle;
 	public double power;
 	public Figure arrow;
-	int[] x;
-	int[] y;
+	private final static int[] ARROW_X_COORD = new int[]{ 0, 10, 10, 50, 50, 10, 10 };
+	private final static int[] ARROW_Y_COORD = new int[]{ 30, 0, 25, 25, 35, 35, 60 };
 
 	public Windtunnel(Double angle) {
+		super(ARROW_X_COORD, ARROW_Y_COORD);
 		this.angle = angle;
 		power = 1;
+		/*
 		int[] xa = { 0, 10, 10, 50, 50, 10, 10 }; //{ 5, 5, 70, 70, 105, 70, 70 }; //
 		int[] ya = { 30, 0, 25, 25, 35, 35, 60 }; //{ 220, 250, 250, 280, 230, 200, 220 };  //
 		x = xa;
 		y = ya;
 		arrow = new Figure(x, y);
-
+		*/
 	}
 
 	public Windtunnel() {
+		this(0.0);
+		/*
 		this.angle = 0;
 		int[] x =  { 0, 10, 10, 50, 50, 10, 10 }; // { 5, 5, 70, 70, 105, 70, 70 }; //
 		int[] y = { 30, 0, 25, 25, 35, 35, 60 }; //{ 220, 250, 250, 280, 230, 200, 220 }; //
 		this.x = x;
 		this.y = y;
 		arrow = new Figure(x, y);
+		*/
 	}
 }
